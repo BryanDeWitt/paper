03400bcd2f38055b78e5aae8c27b80c386ca73ae
Bryan <tolovehayley@gmail.com>
2023-02-12 13:42:41 -0300

    added the buttons and event listeners

--- paper.js
+++ paper.js
@@ -1,29 +1,53 @@
-const choices = ["rock", "paper", "scissors"]
-let playerScore = 0
-let pcScore = 0
-function playRound(choice1, choice2){
-    if (choice1 === "rock" && choice2 === "scissors") {
-        console.log(`You win ${choice1} beats ${choice2}`)
-        playerScore++
-    }else if(choice1 === "scissors" && choice2 === "paper"){
-        console.log(`You win ${choice1} beats ${choice2}`)
-        playerScore++
-    }else if(choice1 === "paper" && choice2 === "rock"){
-        console.log(`You win ${choice1} beats ${choice2}`)
-        playerScore++
-    }else if(choice1 === choice2){
-        console.log("Draw")
-    }else{
-        console.log(`You lose ${choice2} beats ${choice1}`)
-        pcScore++
+document.addEventListener('DOMContentLoaded', function(){
+    const choices = ["rock", "paper", "scissors"]
+    let playerScore = 0
+    let pcScore = 0
+    const div = document.querySelector('#container');
+    const pc = document.querySelector('#pc');
+    const player = document.querySelector('#player');
+    function playRound(choice1, choice2){
+        if (choice1 === "rock" && choice2 === "scissors") {
+            div.textContent = `You win ${choice1} beats ${choice2}`;
+            playerScore++
+            pc.textContent = `PC score: ${pcScore}`;
+            player.textContent = `Your score: ${playerScore}`;
+        }else if(choice1 === "scissors" && choice2 === "paper"){
+            div.textContent = `You win ${choice1} beats ${choice2}`;
+            playerScore++
+            pc.textContent = `PC score: ${pcScore}`;
+            player.textContent = `Your score: ${playerScore}`;
+        }else if(choice1 === "paper" && choice2 === "rock"){
+            div.textContent = `You win ${choice1} beats ${choice2}`;
+            playerScore++
+            pc.textContent = `PC score: ${pcScore}`;
+            player.textContent = `Your score: ${playerScore}`;
+        }else if(choice1 === choice2){
+            div.textContent = "Draw";
+            pc.textContent = `PC score: ${pcScore}`;
+            player.textContent = `Your score: ${playerScore}`;
+        }else{
+            div.textContent = `You win ${choice1} beats ${choice2}`;
+            pcScore++
+            pc.textContent = `PC score: ${pcScore}`;
+            player.textContent = `Your score: ${playerScore}`;
+        }
     }
-}
-while(true){
-    let pcChoice = choices[Math.ceil(Math.random() * choices.length) - 1]
-    let playerChoice = prompt("Rock, paper or scissors?").toLowerCase()
-    playRound(playerChoice, pcChoice)
-    if(playerScore === 5|| pcScore === 5) break
-}
+    
+    const buttons = Array.from(document.querySelectorAll('button'));
+    let pcChoice = () => choices[Math.ceil(Math.random() * choices.length) - 1]
+    let playerChoice;
+    function letsPlay(){
+        buttons.forEach(choice => choice.addEventListener('click', (e) =>{
+            playerChoice = e.target.id;
+            playRound(playerChoice, pcChoice());
+        }))
+    }
+    
 
-if(playerScore === 5) console.log("You Win");
-else console.log("You Lose");
+    letsPlay()
+    
+        
+    
+
+    
+})
